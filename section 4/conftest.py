c0d0a43742de4500e91b9b0d70049c93dd989511
Danil_Farafonov <faraphonovd@yandex.ru>
2023-07-21 19:05:39 +0500

    lesson2_step6 add base and main pages, add simple tests

---
+++ section 4/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chromeOptions
+from selenium.webdriver.firefox.options import Options as firefoxOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help="language for browser")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+
+    browser = None
+
+    if browser_name == "chrome":
+        options = chromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        options = firefoxOptions()
+        options.set_preference('intl.accept_languages', language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
