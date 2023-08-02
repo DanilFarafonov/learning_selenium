d5d2f4aa72bb12601f9ba7e63d559fbeb0cce035
Danil_Farafonov <faraphonovd@yandex.ru>
2023-08-02 23:33:22 +0500

    lesson3_step8 переносит ссылку на страницу авторизации в базовую страницу, добавляет тесты на переход на страницу авторизации с продуктовой

--- section 4/pages/base_page.py
+++ section 4/pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 import math
 
 
@@ -9,6 +10,13 @@ class BasePage:
         self.browser = browser
         self.url = url
         #self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Ссылка на страницу авторизации отсутствует"
 
     def open(self):
         self.browser.get(self.url)
