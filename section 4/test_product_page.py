54898cdad74ae5f1f03e1f880026737666ff74e4
Danil_Farafonov <faraphonovd@yandex.ru>
2023-08-02 20:59:28 +0500

    lesson3_step4 добавляет параметризацию урлов с акцией и помечает один как xfail

--- section 4/test_product_page.py
+++ section 4/test_product_page.py
@@ -1,9 +1,24 @@
+import pytest
+
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize('promo_url',
+                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                          pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo"
+                                       "=offer7", marks=pytest.mark.xfail(reason="чинить это не будут ¯\_(ツ)_/¯")),
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+                          ]
+                         )
+def test_guest_can_add_product_to_basket(browser, promo_url):
+    page = ProductPage(browser, promo_url)
     page.open()
     page.should_be_product_page()
     page.add_product_to_basket()
